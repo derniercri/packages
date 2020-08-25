d73207cde8477a54347d9f92879152a5b57c7877
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2020-08-25 09:44:13 +0200

    :wrench: Safety in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,14 +6,14 @@ yarn gitmoji-changelog
 git stash pop stash@{0}
 git add CHANGELOG.md
 
-TAG_BEFORE_AMEND=`git tag --points-at HEAD`
+TAG_BEFORE_AMEND=$(git tag --points-at HEAD)
 echo "Tag before commit ${TAG_BEFORE_AMEND}"
 
 git commit --amend --no-edit
-COMMIT=`git rev-parse HEAD`
+COMMIT=$(git rev-parse HEAD)
 echo "Commit ${COMMIT}"
 
-git tagm $TAG_BEFORE_AMEND $COMMIT
+git tagm "$TAG_BEFORE_AMEND" "$COMMIT"
 git push --follow-tags
 
 yarn lerna publish from-git --yes
