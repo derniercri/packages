5401cef467f71abde18314c3a125ce1adb422409
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2020-08-29 14:00:55 +0200

    :white_check_mark: Fix tests

--- packages/lerna-gitmoji-changelog/src/parser.test.ts
+++ packages/lerna-gitmoji-changelog/src/parser.test.ts
@@ -1,4 +1,12 @@
+import * as git from "./git";
 import { parseLog } from "./parser";
+
+const getLongHash = jest.fn(() => `abcdef123456`);
+const getFilesInHash = jest.fn(() => ["packages/toto"]);
+
+jest.mock("./git");
+(git as any).getLongHash = getLongHash;
+(git as any).getFilesInHash = getFilesInHash;
 
 describe("Parser", function () {
   describe("parseLog", () => {
@@ -32,61 +40,39 @@ packages/eslint-config-react-native/__tests__/index.test.js
       const expectedResult: ReturnType<typeof parseLog> = [
         {
           hash: "ac6fdcd",
-          longHash: 'ac6fdcdf4616e857801095e255064f966b519724',
+          longHash: "abcdef123456",
           gitmoji: ":bookmark:",
           gitmojiName: "bookmark",
           commit: "Publish v3.3.4",
-          files: [
-            "CHANGELOG.md",
-            "lerna.json",
-            "packages/cli-tools/package.json",
-            "packages/eslint-config-react-native/package.json",
-            "packages/eslint-plugin-i18n/package.json",
-            "packages/react-native-template-derniercri/package.json",
-          ],
-          packageFiles: [
-            "packages/cli-tools/package.json",
-            "packages/eslint-config-react-native/package.json",
-            "packages/eslint-plugin-i18n/package.json",
-            "packages/react-native-template-derniercri/package.json",
-          ],
+          files: ["packages/toto"],
+          packageFiles: ["packages/toto"],
         },
         {
           hash: "2458c93",
-          longHash: '2458c9391ecf8bfdcca540e186e7ad50efca70ff',
+          longHash: "abcdef123456",
           gitmoji: ":white_check_mark:",
           gitmojiName: "white_check_mark",
           commit: "Better tests",
-          files: [
-            "packages/eslint-config-react-native/__tests__/index.test.js",
-            "packages/eslint-config-react-native/example/recommended-example.tsx",
-            "packages/eslint-config-react-native/src/configs/default.js",
-            "packages/react-native-template-derniercri/template/_eslintrc.js",
-          ],
-          packageFiles: [
-            "packages/eslint-config-react-native/__tests__/index.test.js",
-            "packages/eslint-config-react-native/example/recommended-example.tsx",
-            "packages/eslint-config-react-native/src/configs/default.js",
-            "packages/react-native-template-derniercri/template/_eslintrc.js",
-          ],
+          files: ["packages/toto"],
+          packageFiles: ["packages/toto"],
         },
         {
           hash: "4cbccfc",
-          longHash: '4cbccfc58f0347227a3a4fb242d0c0a395d1a40a',
+          longHash: "abcdef123456",
           gitmoji: ":wrench:",
           gitmojiName: "wrench",
           commit: "Better TypeScript coding",
-          files: [".eslintrc", "package.json", "tsconfig.json", "yarn.lock"],
-          packageFiles: [],
+          files: ["packages/toto"],
+          packageFiles: ["packages/toto"],
         },
         {
           hash: "e6138ef",
-          longHash: 'e6138ef2c1ce58612cc7dcc66e63ae7087061d07',
+          longHash: "abcdef123456",
           gitmoji: ":white_check_mark:",
           gitmojiName: "white_check_mark",
           commit: "Fixed tests",
-          files: ["packages/eslint-config-react-native/__tests__/index.test.js"],
-          packageFiles: ["packages/eslint-config-react-native/__tests__/index.test.js"],
+          files: ["packages/toto"],
+          packageFiles: ["packages/toto"],
         },
       ];
       expect(parseLog(snippet)).toEqual(expectedResult);
