11c956659ee3d5f04aea2653fdac2888a62d905f
Donovan BENFOUZARI <donovan@derniercri.io>
2020-09-03 12:08:32 +0200

    💚 Fix CI and make it pass (#22)
    * :green_heart: Fix CI
    
    * :rotating_light: Fix lint
    
    * :white_check_mark: Fix tests by mocking react-navigation
    
    * :white_check_mark: Mock files
    
    * :white_check_mark: Add test utils

--- packages/react-native-template-derniercri/template/src/App.tsx
+++ packages/react-native-template-derniercri/template/src/App.tsx
@@ -1,20 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+import { Provider } from 'react-redux'
 
 import StorybookUIRoot from '../storybook/index'
 
 import env from './modules/env'
 import Home from './screens/home'
+import store from './store'
 
 const Stack = createStackNavigator()
 
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <Provider store={store}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </Provider>
 )
 
 export default env.IS_STORYBOOK === 'true' ? StorybookUIRoot : App
